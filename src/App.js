b486a9b754f9565c4bde6d467dae43ba244924eb
ogkunald <kunaldongare111@gmail.com>
2022-01-08 19:03:39 +0530

    Desktop View for JBC done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
+import { useEffect,useState } from "react";
+import axios from "axios";
 import { JobBarComponent } from "./components";
+
 function App() {
+  const [data, setData] = useState();
+  const URL = "http://localhost:3004/jobs";
+  
+  const getAllContacts = async () => {
+      const response = await axios.get(URL)
+               .catch((error) => {
+                   console.log("Couldn't fetch the data "+ error);
+               })
+      setData(response.data);
+  }
+  
+  useEffect(() => {
+  getAllContacts();
+  }, [URL])
+
+  console.log(data);
   return (
-        
-         <div className="App">
-           <JobBarComponent>
-             
-           </JobBarComponent>
-         </div>
-        
+        <div className="App"> 
+            <header className="bg-teal-500 mb-16 w-full h-full">
+              <img className="object-cover w-full h-fit " src="/images/bg-header-desktop.svg" />  
+            </header> 
+            {!data && <p> Fetching</p>}
+            {data && 
+            <div>
+                  {data.map((job) => {
+                       return <JobBarComponent job = {job} key = {job.id}></JobBarComponent>
+                  })}
+            </div>}
+
+        </div>
   );
 }
 export default App;
--- src/components/JobBarComponent.jsx
+++ src/components/JobBarComponent.jsx
@@ -1,31 +1,37 @@
 import React from 'react'
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 
-const JobBarComponent = () => {
-
-    const [data, setData] = useState();
-    const URL = "http://localhost:3004/jobs";
-
-
-    useEffect(() => {
+const JobBarComponent = (props) => {
     
-    const getAllContacts = async () => {
-        const response = await axios.get(URL)
-                 .catch((error) => {
-                     console.log("Couldn't fetch the data "+ error);
-                 })
-        setData(response.data);
+    const {job,id} = props;
+    const langAndTools = [job.role,job.level];
+    if (job.tools) {
+        langAndTools.push(...job.tools)
     }
-    getAllContacts();
+    if (job.languages) {
+        langAndTools.push(...job.languages)
+    }
 
-    }, [URL])
- 
-    console.log(data);
     return (
-        <div>
-            JBC
+        <div className='flex bg-white shadow-md p-4 mx-48 my-4'>
+            <div>
+                <img src={job.logo} alt={job.company} />
+            </div>
+            <div className='flex flex-col ml-4 justify-between'>
+                <h3 className='text-teal-500 font-bold'>
+                    {job.company}
+                    {job.new && <label className='p-1 border-2 border-teal-600 rounded-full mx-2 font-bold text-xs text-white bg-teal-600'>NEW!</label>}
+                    {job.featured && <label className='p-1 border-2 border-black rounded-full  font-bold text-xs text-white bg-black'>FEATURED</label>}
+                </h3>
+                <h2 className='font-bold text-lg'>{job.position}</h2>
+                <p className='text-gray-500'>
+                    {job.postedAt} · {job.contract} · {job.location}
+                </p>
+            </div>
+            <div className='flex items-center ml-auto '>
+               {langAndTools.length !== 0 && langAndTools.map((lAt,idx)=>(<span className='text-teal-500 bg-teal-100 font-bold m-2 p-1 text-sm rounded' key={idx}>{lAt}</span>))} 
+            </div>
         </div>
+
     )
 }
 
