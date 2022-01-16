09ec35a3c06860f15f69a7d5b71ab90ec6026e89
ogkunald <kunaldongare111@gmail.com>
2022-01-16 21:47:50 +0530

    SearchBoxAdded

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import axios from "axios";
 import { JobBarComponent } from "./components";
 
 function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
+  const [filters, setFilters] = useState([]);
   const URL = "http://localhost:3004/jobs";
-  
   const getAllContacts = async () => {
       const response = await axios.get(URL)
                .catch((error) => {
@@ -16,19 +16,63 @@ function App() {
   
   useEffect(() => {
   getAllContacts();
+  
   }, [URL])
 
-  console.log(data);
+  const filterFunction = (job) =>{
+    if (filters.length === 0) {
+      return true;
+    }
+    const langAndTools = [job.role,job.level];
+    if (job.tools) {
+        langAndTools.push(...job.tools)
+    }
+    if (job.languages) {
+        langAndTools.push(...job.languages)
+    }
+
+    return langAndTools.some((tag) => filters.includes(tag));
+  };
+  
+  const handleTagClick = (tag) => {
+    if(!filters.includes(tag)){
+      setFilters([...filters,tag]);
+    }  
+  }
+  const handleFilterClick = (passedFilter) =>{
+    setFilters(filters.filter((f)=> f !== passedFilter))
+  }
+
+  const filteredJobs = data.filter(filterFunction);
+  
+  
+  console.log(filteredJobs);
+  
   return (
         <div className="App"> 
             <header className="bg-teal-500 mb-16 w-full h-full">
-              <img className="object-cover w-full h-fit " src="/images/bg-header-desktop.svg" />  
-            </header> 
+              <img className="object-cover w-full h-40 " src="/images/bg-header-desktop.svg" />  
+            </header>
+            {filters.length > 0 && (
+            <div className="flex flex-wrap bg-white shadow-md p-6 mx-10 -mt-24 rounded z-10 relative sm:mx-48 sm:p-4">
+              {filters.length > 0 && filters.map((filter)=>
+              <span className="font-semibold cursor-pointer my-4 mx-2  text-sm rounded">
+              <span  className=" text-teal-500 bg-teal-100 p-2 font-bold text-base">{filter}
+              </span>
+              <span className="bg-teal-500 text-teal-50 p-2  font-bold text-base"
+              onClick={()=> handleFilterClick(filter)}
+              >X</span>
+              </span>
+              )}
+              <span className= "text-teal-500 font-bold flex items-center  ml-auto hover:underline " onClick ={() => setFilters([])}>Clear</span> 
+            </div> ) }
             {!data && <p> Fetching</p>}
-            {data && 
+            {filteredJobs && 
             <div>
-                  {data.map((job) => {
-                       return <JobBarComponent job = {job} key = {job.id}></JobBarComponent>
+                  {filteredJobs.map((job) => {
+                       return <JobBarComponent job = {job} key = {job.id} 
+                       handleTagClick = {handleTagClick}
+                       ></JobBarComponent>
                   })}
             </div>}
 
--- src/components/JobBarComponent.jsx
+++ src/components/JobBarComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const JobBarComponent = (props) => {
     
-    const {job,id} = props;
+    const {job,id,handleTagClick} = props;
     const langAndTools = [job.role,job.level];
     if (job.tools) {
         langAndTools.push(...job.tools)
@@ -12,23 +12,23 @@ const JobBarComponent = (props) => {
     }
 
     return (
-        <div className='flex bg-white shadow-md p-4 mx-48 my-4'>
-            <div>
-                <img src={job.logo} alt={job.company} />
+        <div className={`flex flex-col bg-white shadow-md p-4 mx-10 my-16 rounded ${job.featured &&  `border-l-4 border-teal-500`} sm:flex-row sm:mx-48 sm:my-6`}>
+            <div >
+                <img  className='-mt-14 ml-2 w-16 h-16 sm:m-2 sm:w-24 sm:h-24' src={job.logo} alt={job.company} />
             </div>
-            <div className='flex flex-col ml-4 justify-between'>
-                <h3 className='text-teal-500 font-bold'>
+            <div className='flex flex-col m-2 pb-2 justify-between border-b-2 sm:border-none sm:ml-6 '>
+                <h3 className=' text-teal-500 font-bold justify-between '>
                     {job.company}
-                    {job.new && <label className='p-1 border-2 border-teal-600 rounded-full mx-2 font-bold text-xs text-white bg-teal-600'>NEW!</label>}
-                    {job.featured && <label className='p-1 border-2 border-black rounded-full  font-bold text-xs text-white bg-black'>FEATURED</label>}
+                    {job.new && <span className=' py-1 px-2  rounded-full m-2 font-bold text-sm text-white bg-teal-600'>NEW!</span>}
+                    {job.featured && <span className=' py-1 px-2 rounded-full font-bold text-sm text-white bg-black'>FEATURED</span>}
                 </h3>
-                <h2 className='font-bold text-lg'>{job.position}</h2>
+                <h2 className='font-bold text-lg mt-2 sm:py-2 sm:mt-0'>{job.position}</h2>
                 <p className='text-gray-500'>
                     {job.postedAt} · {job.contract} · {job.location}
                 </p>
             </div>
-            <div className='flex items-center ml-auto '>
-               {langAndTools.length !== 0 && langAndTools.map((lAt,idx)=>(<span className='text-teal-500 bg-teal-100 font-bold m-2 p-1 text-sm rounded' key={idx}>{lAt}</span>))} 
+            <div className='flex flex-wrap  items-center sm:ml-auto'>
+               {langAndTools.length !== 0 && langAndTools.map((lAt,idx)=>(<span className='text-teal-500 bg-teal-100 font-semibold m-2 p-1 text-sm rounded ' key={idx} onClick={() =>handleTagClick(lAt) }>{lAt}</span>))} 
             </div>
         </div>
 
